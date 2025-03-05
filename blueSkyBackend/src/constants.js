e338785e3898b46aea0c8fe73d712c7395b9a86d
developerakashy <developerakashy@gmail.com>
2025-03-05 21:20:08 +0530

    update options and import dotenv

--- blueSkyBackend/src/constants.js
+++ blueSkyBackend/src/constants.js
@@ -1,8 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 const DB_NAME = 'blueSky'
 
 const options = {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production'
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
+    maxAge: 24 * 60 * 60 * 1000,
 }
 
 export { DB_NAME, options }
